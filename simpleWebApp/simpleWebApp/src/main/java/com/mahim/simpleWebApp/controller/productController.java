af639b8802fa74f2b54f361e9e94047f427c583c
Shahriar Jubair <159669661+Shahriar127@users.noreply.github.com>
2024-09-06 18:01:27 +0600

    Update productController.java

--- simpleWebApp/simpleWebApp/src/main/java/com/mahim/simpleWebApp/controller/productController.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/mahim/simpleWebApp/controller/productController.java
@@ -4,36 +4,35 @@ import com.mahim.simpleWebApp.Model.Product;
 import com.mahim.simpleWebApp.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.List;
 @RestController
+@RequestMapping("/product")
 public class productController {
     @Autowired
     ProductService service;
 
-    @GetMapping("/products")
+    @GetMapping
     public List<Product> getProducts() {
         return service.getProducts();
     }
 
-    @GetMapping("/products/{prodId}")
+    @GetMapping("{prodId}")
     public Product getProductById(@PathVariable int prodId) {
         return service.getProductById(prodId);
     }
 
 
-    @PostMapping("/products")
+    @PostMapping
     public void addProduct(@RequestBody Product prod) {
         service.addProduct(prod);
     }
 
-    @PutMapping("/products")
+    @PutMapping
     public void updateProduct(@RequestBody Product prod) {
         service.updateProduct(prod);
     }
 
-    @DeleteMapping("/products/{prodId}")
+    @DeleteMapping("{prodId}")
     public void deleteProduct(@PathVariable int prodId) {
         service.deleteProduct(prodId);
     }
